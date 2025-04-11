9152faf3f37e2e6d445fcdac0fa766385cf9750b
Helmi Pradita <o-helmi.pradita@assarent.co.id>
2025-04-11 13:44:10 +0700

    add: create cluster configuration for express health check app

---
+++ ecosystem.cluster.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "express-health-check-cluster",
+      script: "./server.js",
+      interpreter: "node",
+      interpreter_args: "--enable-source-maps",
+      exec_mode: "cluster",
+      instances: "max",
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+        PORT: 4000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 4000,
+      },
+    },
+  ],
+};
